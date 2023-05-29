c38de829fdff43ac74f7ce99faf80b11125e0f58
https://github.com/Din1998 <dinislam8803@gmail.com>
2023-05-29 14:26:01 +0600

    add modal

--- src/components/liveBoard.js
+++ src/components/liveBoard.js
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil,faFilePdf,faDownload,faArrowRight,faFaceSmile} from '@fortawesome/free-solid-svg-icons'
+import ModalView from './modal'
 
 
 export default function LiveBoard(){
   return(
     <div className="liveBoard__component">
-      <h1>Live Preview</h1>
+      <div className='row'>
+        <div className='col'>
+        <h1>Live Preview</h1>
+        </div>
+        <div className='col'>
+        
+        <ModalView />
+        </div>
+       
+      </div>
+      
         <div className="live__board">
 
         </div>
